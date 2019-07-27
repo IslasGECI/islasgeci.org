b064cb1a6c284490d66abfd69fe7867050a3db85
Ciencia de Datos <analista@islas.org.mx>
2019-07-27 20:35:30 +0000

    Corre contenedor para notebook de NERD

--- src/run_docker.sh
+++ src/run_docker.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #
 # Corre los contenedores de Docker del servidor islasgeci.org
+docker run --detach --publish 8080:8888 --rm --volume ${HOME}/nerd-notebooks/:/workdir islasgeci/jupyter:3691
 docker run --detach --publish 8888:8888 --rm --volume ${HOME}/notebooks/:/workdir islasgeci/jupyter:3691
+

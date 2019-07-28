831cb34e4834be6b0ed919531ddab509a24b433a
Ciencia de Datos <analista@islas.org.mx>
2019-07-28 03:31:47 +0000

    Agrega Tablero de reproducinilidad

--- src/run_docker.sh
+++ src/run_docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # Corre los contenedores de Docker del servidor islasgeci.org
-docker run --detach --publish 8080:8888 --rm --volume ${HOME}/nerd-notebooks/:/workdir islasgeci/jupyter:3691
-docker run --detach --publish 8888:8888 --rm --volume ${HOME}/notebooks/:/workdir islasgeci/jupyter:3691
+docker run --detach --publish 5000:5000 --rm --volume ${HOME}/repositorios/tablero/:/workdir tablero
+docker run --detach --publish 8080:8888 --rm --volume ${HOME}/nerd-notebooks/:/workdir       islasgeci/jupyter:3691
+docker run --detach --publish 8888:8888 --rm --volume ${HOME}/notebooks/:/workdir            islasgeci/jupyter:3691
 

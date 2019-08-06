4c80299598a0183a0b301d78c02ebf31fb4ae87a
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2019-08-06 15:21:38 -0700

    Actualiza `run_docker.sh` con versión de develop

--- src/run_docker.sh
+++ src/run_docker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 # Corre los contenedores de Docker del servidor islasgeci.org
-docker run --detach --publish 5000:5000 --rm --volume ${HOME}/repositorios/tablero/:/workdir tablero
-docker run --detach --publish 8080:8888 --rm --volume ${HOME}/nerd-notebooks/:/workdir       islasgeci/jupyter:3691
-docker run --detach --publish 8888:8888 --rm --volume ${HOME}/notebooks/:/workdir            islasgeci/jupyter:3691
+docker run --detach --publish 5000:5000 --rm --volume ${HOME}/repositorios/tablero_front/:/workdir islasgeci/tablero_front
+docker run --detach --publish 500:5000  --rm --volume ${HOME}/repositorios/tablero_api/:/workdir   islasgeci/tablero_api
+docker run --detach --publish 8080:8888 --rm --volume ${HOME}/nerd-notebooks/:/workdir             islasgeci/jupyter:3691
+docker run --detach --publish 8888:8888 --rm --volume ${HOME}/notebooks/:/workdir                  islasgeci/jupyter:3691
 

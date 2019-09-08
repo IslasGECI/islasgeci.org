fe29ba04c6e7561369acc314446b0e1a9e6b2584
Ciencia de Datos <analista@islas.org.mx>
2019-09-08 08:46:17 -0700

    Actualiza imágen antes de reiniciar contenedor

--- src/start_containers.sh
+++ src/start_containers.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 #
 # Corre los contenedores de Docker del servidor islasgeci.org
+#
+# Actualiza imágenes:
+docker pull islasgeci/tablero_api:latest
 # Con volumen:
 docker run --detach --publish 251:80    --rm --volume ${HOME}/repositorios/gatos-trampas:/go/src/bitbucket.org/IslasGECI/gatos-trampas gatos-trampas ./mapa-gatos
-docker run --detach --publish 500:5000  --rm --volume ${HOME}/repositorios/tablero_api:/workdir   islasgeci/tablero_api
+docker run --detach --publish 500:5000  --rm --volume ${HOME}/repositorios/tablero_api:/workdir   islasgeci/tablero_api:latest
 docker run --detach --publish 8888:8888 --rm --volume ${HOME}/notebooks:/workdir                  islasgeci/jupyter
 # Sin volumen:
 docker run --detach --publish 5000:80   --rm islasgeci/tablero_front

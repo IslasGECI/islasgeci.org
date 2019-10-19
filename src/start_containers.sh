51fdd0f7a5f1683a076d7007064667ba2c4b602d
Ubuntu <ciencia_datos@todos.klcmtt1nj2sexm1aix5afgfh3c.xx.internal.cloudapp.net>
2019-10-19 00:35:53 +0000

    Reordena imágenes

--- src/start_containers.sh
+++ src/start_containers.sh
@@ -4,17 +4,18 @@
 #
 # Variables:
 PATH_TO_REPOS=${HOME}/repositorios
-# Actualiza imágenes:
+# Actualiza imágenes con volumen):
 docker pull islasgeci/gatos-trampas:latest
-docker pull islasgeci/jupyter:latest
-docker pull islasgeci/nerd_demo:latest
 docker pull islasgeci/tablero_api:latest
+docker pull islasgeci/jupyter:latest
+# Actualiza imágenes sin volumen):
 docker pull islasgeci/tablero_front:latest
-# Con volumen:
+docker pull islasgeci/nerd_demo:latest
+# Corre contenedores con volumen:
 docker run --detach --publish  251:80   --rm --volume gatos-trampas_vol:/go/src/bitbucket.org/IslasGECI/gatos-trampas islasgeci/gatos-trampas:latest ./mapa-gatos
 docker run --detach --publish  500:5000 --rm --volume tablero_api_vol:/workdir islasgeci/tablero_api:latest
 docker run --detach --publish 8888:8888 --rm --volume jupyter_vol:/workdir islasgeci/jupyter:latest
-# Sin volumen:
+# Corre contenedores sin volumen:
 docker run --detach --publish 5000:80   --rm islasgeci/tablero_front:latest
 docker run --detach --publish 8080:8888 --rm islasgeci/nerd_demo:latest
 #

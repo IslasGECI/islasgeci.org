2590975bbe3795634c24d1772c28e14af3640ff8
Ubuntu <ciencia_datos@todos.klcmtt1nj2sexm1aix5afgfh3c.xx.internal.cloudapp.net>
2019-10-31 15:09:10 -0700

    Agrega imágenes de mapa de aves marinas

--- src/start_containers.sh
+++ src/start_containers.sh
@@ -4,19 +4,24 @@
 #
 # Variables:
 PATH_TO_REPOS=${HOME}/repositorios
-# Actualiza imágenes con volumen):
+# Actualiza imágenes:
 docker pull islasgeci/gatos-trampas:latest
-docker pull islasgeci/tablero_api:latest
-docker pull islasgeci/jupyter:latest
-# Actualiza imágenes sin volumen):
 docker pull islasgeci/homepage:latest
-docker pull islasgeci/tablero_front:latest
+docker pull islasgeci/jupyter:latest
 docker pull islasgeci/nerd_demo:latest
+docker pull islasgeci/tablero_api:latest
+docker pull islasgeci/tablero_front:latest
+docker pull islasgeci/tamanio-poblacional-aves-marinas_api-datos:latest
+docker pull islasgeci/tamanio-poblacional-aves-marinas_api-lambdas:latest
+docker pull islasgeci/tamanio-poblacional-aves-marinas_front:latest
 # Corre contenedores con volumen:
 docker run --detach --publish  251:80   --rm --volume gatos-trampas_vol:/go/src/bitbucket.org/IslasGECI/gatos-trampas islasgeci/gatos-trampas:latest ./mapa-gatos
 docker run --detach --publish  500:5000 --rm --volume tablero_api_vol:/workdir islasgeci/tablero_api:latest
 docker run --detach --publish 8888:8888 --rm --volume jupyter_vol:/workdir islasgeci/jupyter:latest
 # Corre contenedores sin volumen:
 docker run --detach --publish   80:80   --rm islasgeci/homepage:latest
+docker run --detach --publish  850:80   --rm islasgeci/tamanio-poblacional-aves-marinas_front
+docker run --detach --publish  851:4000 --rm islasgeci/tamanio-poblacional-aves-marinas_api-datos
+docker run --detach --publish  852:5000 --rm islasgeci/tamanio-poblacional-aves-marinas_api-lambdas
 docker run --detach --publish 5000:80   --rm islasgeci/tablero_front:latest
 docker run --detach --publish 8080:8888 --rm islasgeci/nerd_demo:latest

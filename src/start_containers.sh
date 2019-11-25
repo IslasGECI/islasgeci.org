926f752dbd7ef31f8c8da2e2ae87a602ad3ee00c
Ubuntu <ciencia_datos@todos.klcmtt1nj2sexm1aix5afgfh3c.xx.internal.cloudapp.net>
2019-11-25 15:04:55 -0800

    Deja de correr el Inspector de Reproducibilidad porque no está terminado

--- src/start_containers.sh
+++ src/start_containers.sh
@@ -7,7 +7,6 @@ docker pull islasgeci/gatos-trampas:latest
 docker pull islasgeci/homepage:latest
 docker pull islasgeci/jupyter:latest
 docker pull islasgeci/nerd_demo:latest
-docker pull islasgeci/reproducibility_inspector:latest
 docker pull islasgeci/tablero_api:latest
 docker pull islasgeci/tablero_front:latest
 docker pull islasgeci/tag_docker_images:latest
@@ -16,7 +15,6 @@ docker pull islasgeci/tamanio-poblacional-aves-marinas_api-lambdas:latest
 docker pull islasgeci/tamanio-poblacional-aves-marinas_front:latest
 docker pull rocker/tidyverse:latest
 # Corre contenedores con volumen:
-docker run --detach                     --rm --volume reproducibility_inspector_vol:/workdir --volume secrets_vol:/.vault --volume /home/ciencia_datos/.ssh:/root/.ssh --volume /var/run/docker.sock:/var/run/docker.sock islasgeci/reproducibility_inspector:latest
 docker run --detach                     --rm --volume secrets_vol:/.vault --volume /var/run/docker.sock:/var/run/docker.sock islasgeci/tag_docker_images:latest
 docker run --detach --publish  251:3000 --rm --volume gatos-trampas_vol:/go/src/bitbucket.org/IslasGECI/gatos-trampas islasgeci/gatos-trampas:latest ./mapa-gatos
 docker run --detach --publish  500:5000 --rm --volume tablero_api_vol:/workdir islasgeci/tablero_api:latest
